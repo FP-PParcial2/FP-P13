abc88869c9842fabfe2d4f93b6052211bcbd385b
Eazuara <A01410412@itesm.mx>
2016-10-20 19:18:56 -0500

    Update FPP13.java

--- FP-P13/src/fp/p13/FPP13.java
+++ FP-P13/src/fp/p13/FPP13.java
@@ -15,21 +15,21 @@ public class FPP13 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Declaracion de variables y Matriz
 int []mat = new int[30];
 int p,mp;
 mat=Datos(mat);
 p=promedio(mat);
 ArribaPromedio(mat,p);
 }
-public static int[] Datos(int[] v){
+public static int[] Datos(int[] v){//Metod para pedir Datos en las posiciones de las 30 matrices
 Scanner entrada = new Scanner(System.in);
 for(int i=0;i<v.length;i++){
 System.out.print("Introduce un numero para la posicion ["+ (i+1) +"] :");
 v[i]=entrada.nextInt();
 }
 return v;}
-public static int promedio(int[] v){
+public static int promedio(int[] v){//Revisar y calcular promedio de la matriz
 int a=0,p=0;
 for (int i=0; i<v.length; i++){
 a=v[i]+a;
@@ -38,15 +38,15 @@ p++;
 System.out.println("El promedio de los numeros en el arreglo es: "+(a/p));
 return (a/p);
 }
-public static void ArribaPromedio(int[] v,int prom){
+public static void ArribaPromedio(int[] v,int prom){//Metodo que checa numeros por arriba y abajo del Prom
 int c=0;
 for(int i=0;i<v.length;i++){
 if(v[i]>prom){
 c++;
 }
 }
-System.out.println("La cantidad de numeros sobre el promedio es: "+c);
-System.out.println("La cantidad de numeros debajo del promedio es: "+(v.length-c));
+System.out.println("La cantidad de numeros sobre el promedio es: "+c);//Imprimir
+System.out.println("La cantidad de numeros debajo del promedio es: "+(v.length-c));//Imprimir
 }
 
 }
